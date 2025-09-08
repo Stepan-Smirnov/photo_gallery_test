31ebc82a15c1876ca3676e9c604954c7d0e5eac3
FakaFakaYeah <fakafaka270695@yandex.ru>
2025-09-08 20:01:10 +0300

    Add Redis dependency and update configuration settings
    - Added Redis as a dependency in pyproject.toml and uv.lock.
    - Updated Settings class in config.py to include Redis configuration options (host, port, and optional password).
    - Added a redis_url property to construct the Redis connection string.
    - Minor reordering of imports in db.py for consistency.

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
 
@@ -8,7 +11,11 @@ class Settings(BaseSettings):
     DB_PORT: str = "5432"
     POSTGRES_DB: str = "postgres"
     POSTGRES_USER: str = "postgres"
-    POSTGRES_PASSWORD: str = "postgres"
+    POSTGRES_PASSWORD: str = "postgres_password"
+
+    REDIS_HOST: str
+    REDIS_PORT: str
+    REDIS_PASSWORD: Optional[str] = None
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
@@ -17,6 +24,10 @@ class Settings(BaseSettings):
         user = f'{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}'
         database = f'{self.DB_HOST}:{self.DB_PORT}/{self.POSTGRES_DB}'
         return f'postgresql+asyncpg://{user}@{database}'
+
+    @property
+    def redis_url(self):
+        return f'redis://{self.REDIS_HOST}:{self.REDIS_PORT}:{self.REDIS_PASSWORD}'
 
 
 settings = Settings()
